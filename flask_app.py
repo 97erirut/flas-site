3ee6b559f2b677ba0407707e87f5a1bc86675eb6
97erirut <erik.zr11@gmail.com>
2015-12-09 15:35:55 +0100

    Update flask_app.py
    Ändring av databasen krävde att jag behövde göra om filen.

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+"""Author: Erik Ruthström, 2015"""
+
+#import flask
+from flask import Flask, render_template
+
+#create flask-app
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/startpage/')
+def startpage():
+    """..."""
+    return render_template("startpage.html")
+
+if (__name__ == "__main__"):
+    app.run
